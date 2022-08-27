c38f4373298ea0da8b71c8db20b8e34247fcedb3
afrodisiac <eric.gericke2@gmail.com>
2022-08-28 00:46:07 +0200

    removed redundant code

--- Code/tile.py
+++ Code/tile.py
@@ -3,47 +3,6 @@ from settings import *
 from helper import *
 import random
 from camera import*
-class Tile(pygame.sprite.Sprite):
-    def __init__(self, group):
-        super().__init__(group)
-        self.tiles = import_images("Graphics/background and map.png")
-        
-class TerrainTile1(Tile):
-    def __init__(self, group, pos):
-        super().__init__(group)
-        self.image= self.tiles[2]
-        self.rect = self.image.get_rect(topleft = pos)
-
-class TerrainTile2(Tile):
-    def __init__(self, group, pos):
-        super().__init__(group)
-        self.image= self.tiles[3]
-        self.rect = self.image.get_rect(topleft = pos)
-
-class Clouds1(Tile):
-    def __init__(self, group, pos):
-        super().__init__(group)
-        self.image= self.tiles[4]
-        self.rect = self.image.get_rect(topleft = pos)
-
-class Clouds2(Tile):
-    def __init__(self, group, pos):
-        super().__init__(group)
-        self.image= self.tiles[5]
-        self.rect = self.image.get_rect(topleft = pos)
-
-class Stars(Tile):
-    def __init__(self, group, pos):
-        super().__init__(group)
-        self.image= self.tiles[1]
-        self.rect = self.image.get_rect(topleft = pos)
-
-class Background(Tile):
-    def __init__(self, group, pos):
-        super().__init__(group)
-        self.image= self.tiles[0]
-        self.rect = self.image.get_rect(topleft = pos)
-
 
 class StaticTile(pygame.sprite.Sprite):
     def __init__(self, group, pos, image):
